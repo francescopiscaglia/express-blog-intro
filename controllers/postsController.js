e64d1704c48af32f8682c3776be9252481c6a302
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-30 15:18:06 +0100

    add /posts root

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,29 +1,15 @@
 // Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
 
-const posts = [
-    {
-        title: "Il mio primo post",
-        content: "Lorem lorem lorem lorem",
-        tags: ["lorem", "lorem", "lorem", "lorem"]
-    },
-    {
-        title: "Il mio secondo post",
-        content: "Lorem lorem lorem lorem",
-        tags: ["lorem", "lorem", "lorem", "lorem"]
-    },
-    {
-        title: "Il mio terzo post",
-        content: "Lorem lorem lorem lorem",
-        tags: ["lorem", "lorem", "lorem", "lorem"]
-    },
-    {
-        title: "Il mio quarto post",
-        content: "Lorem lorem lorem lorem",
-        tags: ["lorem", "lorem", "lorem", "lorem"]
-    },
-    {
-        title: "Il mio quinto post",
-        content: "Lorem lorem lorem lorem",
-        tags: ["lorem", "lorem", "lorem", "lorem"]
-    },
-]
+const posts = require("../data/posts.js");
+
+function index(req, res) {
+
+    res.json({
+        data: posts,
+        count: posts.length
+    });
+};
+
+module.exports = {
+    index
+};
---
+++ data/posts.js
@@ -0,0 +1,29 @@
+const posts = [
+    {
+        title: "Il mio primo post",
+        content: "Lorem lorem lorem lorem",
+        tags: ["lorem", "lorem", "lorem", "lorem"]
+    },
+    {
+        title: "Il mio secondo post",
+        content: "Lorem lorem lorem lorem",
+        tags: ["lorem", "lorem", "lorem", "lorem"]
+    },
+    {
+        title: "Il mio terzo post",
+        content: "Lorem lorem lorem lorem",
+        tags: ["lorem", "lorem", "lorem", "lorem"]
+    },
+    {
+        title: "Il mio quarto post",
+        content: "Lorem lorem lorem lorem",
+        tags: ["lorem", "lorem", "lorem", "lorem"]
+    },
+    {
+        title: "Il mio quinto post",
+        content: "Lorem lorem lorem lorem",
+        tags: ["lorem", "lorem", "lorem", "lorem"]
+    },
+];
+
+module.exports = posts;
