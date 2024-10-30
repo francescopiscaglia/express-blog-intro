15d69a6c9e2f097b8c270828e29169461967994e
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-30 16:08:48 +0100

    add dynamic markup

--- data/posts.js
+++ data/posts.js
@@ -2,31 +2,31 @@ const posts = [
     {
         title: "Il mio primo post",
         content: "Lorem lorem lorem lorem",
-        img: "../public/img/image1.png",
+        img: "http://127.0.0.1:3001/img/image1.png",
         tags: ["lorem", "lorem", "lorem", "lorem"]
     },
     {
         title: "Il mio secondo post",
         content: "Lorem lorem lorem lorem",
-        img: "../public/img/image2.png",
+        img: "http://127.0.0.1:3001/img/image2.png",
         tags: ["lorem", "lorem", "lorem", "lorem"]
     },
     {
         title: "Il mio terzo post",
         content: "Lorem lorem lorem lorem",
-        img: "../public/img/image3.png",
+        img: "http://127.0.0.1:3001/img/image3.png",
         tags: ["lorem", "lorem", "lorem", "lorem"]
     },
     {
         title: "Il mio quarto post",
         content: "Lorem lorem lorem lorem",
-        img: "../public/img/image4.png",
+        img: "http://127.0.0.1:3001/img/image4.png",
         tags: ["lorem", "lorem", "lorem", "lorem"]
     },
     {
         title: "Il mio quinto post",
         content: "Lorem lorem lorem lorem",
-        img: "../public/img/image5.png",
+        img: "http://127.0.0.1:3001/img/image4.png",
         tags: ["lorem", "lorem", "lorem", "lorem"]
     },
 ];
--- public/js/main.js
+++ public/js/main.js
@@ -8,15 +8,29 @@ fetch("http://127.0.0.1:3001/posts")
         // mi salvo l'array di posts
         const posts = data.data;
 
+        let gallery = "";
+
         // ciclo dentro l'array
         posts.forEach(post => {
 
+            // destrotturo per poter accedere ad ogni chiave
             const { title, img, content, tags } = post;
-            
-        });
         
+            // mi creo il markup
+            const markup = `
+                <li>
+                    <img src="${img}" alt="">
+                    <h3>${title}</h3>
+                    <p>${content}</p>
+                    <a href="#">${tags}</a>
+                </li>
+            `;
 
-        
-        
+            // aggiungo il markup alla variabile ad ogni iterazione
+            gallery += markup;
+        });
+
+        // aggiorno la DOM
+        ulEl.innerHTML = gallery;
     })
     .catch(error => console.error(error));
